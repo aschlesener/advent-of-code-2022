c823b0671cfefd00c0a3f8a2544efc0beb7e7344
Amy Schlesener <amy@amyschlesener.com>
2022-12-01 00:13:47 -0800

    Add C# project

---
+++ AdventOfCode/AdventOfCode2022/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode2022
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Specify a day to run by passing in an argument, e.g. '1'");
+                return;
+            }
+
+            int day = int.Parse(args[0]);
+            switch (day)
+            {
+                case 1:
+                    Day1.Run();
+                    break;
+                default:
+                    Console.WriteLine("Enter a number between 1 and 1.");
+                    break;
+            }
+        }
+    }
+}
